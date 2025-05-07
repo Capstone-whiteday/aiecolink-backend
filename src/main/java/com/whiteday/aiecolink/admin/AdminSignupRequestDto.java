50d8d828a7549db70ce141a556a5eff81a2c93ca
nova0128 <glanda28@naver.com>
2025-05-07 17:07:29 +0900

    feat: 관리자 회원가입 기능 추가

---
+++ src/main/java/com/whiteday/aiecolink/admin/AdminSignupRequestDto.java
@@ -0,0 +1,10 @@
+package com.whiteday.aiecolink.admin;
+
+import lombok.Getter;
+
+@Getter
+public class AdminSignupRequestDto {
+    private String name;
+    private String email;
+    private String password;
+}
