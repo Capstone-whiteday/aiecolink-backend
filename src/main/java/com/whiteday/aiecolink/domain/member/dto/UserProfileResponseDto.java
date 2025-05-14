d953494b849c8c8e5ffc2392221ff6436b51b868
nova0128 <glanda28@naver.com>
2025-05-14 17:28:12 +0900

    feat: 회원 프로필 조회 및 수정 기능 추가

---
+++ src/main/java/com/whiteday/aiecolink/domain/member/dto/UserProfileResponseDto.java
@@ -0,0 +1,10 @@
+package com.whiteday.aiecolink.domain.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserProfileResponseDto {
+    private String name;
+}
