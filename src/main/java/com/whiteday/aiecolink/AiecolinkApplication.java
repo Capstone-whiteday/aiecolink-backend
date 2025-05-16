c411d76d14aa946043499efe8e01c68e50627b2e
Subin Choi <125132754+subi930@users.noreply.github.com>
2025-05-16 11:43:08 +0900

    feat : enable scheduling support in the application

--- src/main/java/com/whiteday/aiecolink/AiecolinkApplication.java
+++ src/main/java/com/whiteday/aiecolink/AiecolinkApplication.java
@@ -2,8 +2,10 @@ package com.whiteday.aiecolink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AiecolinkApplication {
 
     public static void main(String[] args) {
