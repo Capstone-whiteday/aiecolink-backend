f7c15c6480362938c6e81ade4f70aeb1fc10e77a
Subin Choi <125132754+subi930@users.noreply.github.com>
2025-05-19 10:49:14 +0900

    feat : Region 데이터 생성 순서 첫번째로 수정

--- src/main/java/com/whiteday/aiecolink/domain/station/model/init/RegionInit.java
+++ src/main/java/com/whiteday/aiecolink/domain/station/model/init/RegionInit.java
@@ -4,8 +4,10 @@ import com.whiteday.aiecolink.domain.station.model.entity.Region;
 import com.whiteday.aiecolink.domain.station.repository.RegionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+@Order(1)
 @Component
 @RequiredArgsConstructor
 public class RegionInit implements CommandLineRunner {
